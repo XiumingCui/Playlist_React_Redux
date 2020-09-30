59d1c8697d5fe5a66b8f1909f80478c53b53bbe3
XM Cui <42259352+XiumingCui@users.noreply.github.com>
2020-09-30 17:27:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+import {
+  FETCH_SONGS,
+  ADD_FAV,
+  REMOVE_FAV,
+  ADD_LIS,
+  REMOVE_LIS,
+} from "../constraints";
+const initialState = {
+  loading: false,
+  songs: [],
+};
+
+const reducers = (state = initialState, action) => {
+  console.log(action.type);
+  switch (action.type) {
+    case FETCH_SONGS:
+      // return {...state, songs: action.payload.data.map(s => {...s, favorited: fasle, listend: fasle})};
+      return { ...state, songs: action.payload.data };
+    case ADD_FAV:
+      console.log(action.type + ' : ' + action.payload);
+      return {
+        ...state,
+        songs: state.songs.map((s) =>
+          s.id === action.payload ? { ...s, favourite: true } : s
+        ),
+      };
+    case REMOVE_FAV:
+      console.log(action.type + ' : ' + action.payload);
+      return {
+        ...state,
+        songs: state.songs.map((s) =>
+          s.id === action.payload ? { ...s, favourite: false } : s
+        ),
+      };
+    case ADD_LIS:
+      return {
+        ...state,
+        songs: state.songs.map((s) =>
+          s.id === action.payload ? { ...s, listened: true } : s
+        ),
+      };
+    case REMOVE_LIS:
+      return {
+        ...state,
+        songs: state.songs.map((s) =>
+          s.id === action.payload ? { ...s, listened: false } : s
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducers;
